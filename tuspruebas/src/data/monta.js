e567ed6092dbd6120b658f3fb8428f739f0ad0c6
Brigitte Blau <brigitteyaelblau@gmail.com>
2025-01-21 00:56:43 -0300

    add data structure for 7th grade subjects and topics with associated images

---
+++ tuspruebas/src/data/monta.js
@@ -0,0 +1,163 @@
+const data = {
+    grade: "7MO GRADO",
+    subjects: [
+      {
+        name: "CIENCIA Y TECNOLOGIA",
+        topics: [
+          {
+            name: "Flujos de un sistema",
+            images: [
+              {
+                src: "OrtMonta/7MO GRADO/tecnologia/flujos de un sistema/1.jpg",
+                type: "Prueba",
+                teacher: "Profesor Pérez"
+              },
+              {
+                src: "OrtMonta/7MO GRADO/tecnologia/flujos de un sistema/2.jpg",
+                type: "Trabajo Práctico",
+                teacher: "Profesor Pérez"
+              },
+              {
+                src: "OrtMonta/7MO GRADO/tecnologia/flujos de un sistema/3.jpg",
+                type: "Prueba",
+                teacher: "Profesor Pérez"
+              }
+            ]
+          }
+        ]
+      },
+      {
+        name: "CIENCIAS NATURALES",
+        topics: [
+          {
+            name: "Evaluación de ecosistemas",
+            images: [
+              {
+                src: "OrtMonta/7MO GRADO/naturales/ecosistema/1.jpg",
+                type: "Prueba",
+                teacher: "Profesora López"
+              },
+              {
+                src: "OrtMonta/7MO GRADO/naturales/ecosistema/2.jpg",
+                type: "Trabajo Práctico",
+                teacher: "Profesora López"
+              },
+              {
+                src: "OrtMonta/7MO GRADO/naturales/ecosistema/3.jpg",
+                type: "Prueba",
+                teacher: "Profesora López"
+              }
+            ]
+          }
+        ]
+      },
+      {
+        name: "CIENCIAS SOCIALES",
+        topics: null
+      },
+      {
+        name: "CULTURA JUDIA (fuentes)",
+        topics: null
+      },
+      {
+        name: "HISTORIA JUDIA",
+        topics: [
+          {
+            name: "Prueba del segundo bimestre",
+            images: [
+              {
+                src: "OrtMonta/7MO GRADO/historia judia/segundo bimestre/1.jpg",
+                type: "Prueba",
+                teacher: "Profesor Cohen"
+              },
+              {
+                src: "OrtMonta/7MO GRADO/historia judia/segundo bimestre/2.jpg",
+                type: "Prueba",
+                teacher: "Profesor Cohen"
+              },
+              {
+                src: "OrtMonta/7MO GRADO/historia judia/segundo bimestre/3.jpg",
+                type: "Prueba",
+                teacher: "Profesor Cohen"
+              }
+            ]
+          }
+        ]
+      },
+      {
+        name: "LENGUA",
+        topics: null
+      },
+      {
+        name: "MATEMATICA",
+        topics: [
+          {
+            name: "Prueba de geometría",
+            images: [
+              {
+                src: "OrtMonta/7MO GRADO/matematica/geometria/1.jpg",
+                type: "Prueba",
+                teacher: "Profesora Martínez"
+              },
+              {
+                src: "OrtMonta/7MO GRADO/matematica/geometria/2.jpg",
+                type: "Prueba",
+                teacher: "Profesora Martínez"
+              },
+              {
+                src: "OrtMonta/7MO GRADO/matematica/geometria/3.jpg",
+                type: "Trabajo Práctico",
+                teacher: "Profesora Martínez"
+              }
+            ]
+          },
+          {
+            name: "Prueba de operaciones con números racionales",
+            images: [
+              {
+                src: "OrtMonta/7MO GRADO/matematica/operacionesNumerosRacionales/1.jpg",
+                type: "Prueba",
+                teacher: "Profesor García"
+              },
+              {
+                src: "OrtMonta/7MO GRADO/matematica/operacionesNumerosRacionales/2.jpg",
+                type: "Prueba",
+                teacher: "Profesor García"
+              },
+              {
+                src: "OrtMonta/7MO GRADO/matematica/operacionesNumerosRacionales/3.jpg",
+                type: "Trabajo Práctico",
+                teacher: "Profesor García"
+              }
+            ]
+          },
+          {
+            name: "Prueba de problemas con números decimales",
+            images: [
+              {
+                src: "OrtMonta/7MO GRADO/matematica/problemasNumerosDecimales/1.jpg",
+                type: "Prueba",
+                teacher: "Profesor Rodríguez"
+              },
+              {
+                src: "OrtMonta/7MO GRADO/matematica/problemasNumerosDecimales/2.jpg",
+                type: "Trabajo Práctico",
+                teacher: "Profesor Rodríguez"
+              },
+              {
+                src: "OrtMonta/7MO GRADO/matematica/problemasNumerosDecimales/3.jpg",
+                type: "Prueba",
+                teacher: "Profesor Rodríguez"
+              },
+              {
+                src: "OrtMonta/7MO GRADO/matematica/problemasNumerosDecimales/4.jpg",
+                type: "Prueba",
+                teacher: "Profesor Rodríguez"
+              }
+            ]
+          }
+        ]
+      }
+    ]
+  };
+  
